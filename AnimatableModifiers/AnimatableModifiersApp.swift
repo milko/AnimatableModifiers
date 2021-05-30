a1707615c02994ceb29f43563200a005e51845bd
Milko Škofič <skofic@gmail.com>
2021-05-30 14:29:59 +0200

    Initial Commit

---
+++ AnimatableModifiers/AnimatableModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatableModifiersApp.swift
+//  AnimatableModifiers
+//
+//  Created by Milko Škofič on 30/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatableModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
